0eab1883870e2df209507b9178bb1c525d907168
David <davidwatsonbr@gmail.com>
2025-09-08 11:21:21 +0000

    subindo exercicio clean code 0

---
+++ 0x00/nomenclatura/UserAccount.java
@@ -0,0 +1,36 @@
+public class UserAccount {
+    public Date modifiedDate;
+    public List<Testcase> findAllTestCasesByUser(User user) {
+        //implementação
+    };
+    public List<Account> accountList;
+
+    public void deleteUser(User user) {
+        //implementação
+    }; 
+
+    public URL url;
+
+    public class TableRepresenter {
+        //implementação
+    };
+
+    public void saveCar() {
+        //implementação
+    };
+
+    public saveUser() {
+        //implementação
+    };
+
+    public saveAccount() {
+        //implementação
+    };
+
+    public saveContract() {
+        //implementação
+    };
+
+    public final int DAYS_IN_YEAR = 360;
+    public final int HOURS_IN_WEEK = 168;
+}
